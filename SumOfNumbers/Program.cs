02b0a65758dec27aa95344e00f9fd96b1da2c13a
RaunakAbhishek25 <abhishekraunak793@gmail.com>
2026-02-17 08:19:27 +0530

    Add files via upload

---
+++ SumOfNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SumOfNumbers
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n, sum = 0;
+
+            Console.WriteLine("Enter number of elements ");
+            n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("Enter number " + i + ": ");
+                int num = int.Parse(Console.ReadLine());
+                sum += num;
+            }
+
+            Console.WriteLine("Sum of numbers = " + sum);
+        }
+    }
+}
