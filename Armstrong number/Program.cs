e214796d4cddc7b248c115bd06d49654e734b737
RaunakAbhishek25 <abhishekraunak793@gmail.com>
2026-02-25 01:20:09 +0530

    Add files via upload

---
+++ Armstrong number/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        int originalNum = num;
+        int sum = 0;
+        int digits = num.ToString().Length;
+
+        while (num > 0)
+        {
+            int digit = num % 10;
+            sum += (int)Math.Pow(digit, digits);
+            num /= 10;
+        }
+
+        if (sum == originalNum)
+            Console.WriteLine($"{originalNum} is an Armstrong number.");
+        else
+            Console.WriteLine($"{originalNum} is not an Armstrong number.");
+    }
+}
