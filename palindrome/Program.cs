dc4a53a55719ed41f6f7bb697315eb56214afb3b
RaunakAbhishek25 <abhishekraunak793@gmail.com>
2026-02-25 01:13:04 +0530

    Add files via upload

---
+++ palindrome/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        int originalNum = num;
+        int reverseNum = 0;
+
+        while (num > 0)
+        {
+            int digit = num % 10;
+            reverseNum = reverseNum * 10 + digit;
+            num = num / 10;
+        }
+
+        if (originalNum == reverseNum)
+            Console.WriteLine("The number is a palindrome.");
+        else
+            Console.WriteLine("The number is not a palindrome.");
+    }
+}
