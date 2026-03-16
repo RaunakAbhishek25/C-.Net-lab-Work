1160c8186fed7cb59b7a1e43565bf13f801b0dc6
RaunakAbhishek25 <abhishekraunak793@gmail.com>
2026-03-16 10:14:03 +0530

    Add files via upload

---
+++ Calculate Sum and Average/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] marks = new int[5];
+        int sum = 0;
+        double average;
+
+        // Input marks
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Enter marks of student " + (i + 1) + ": ");
+            marks[i] = Convert.ToInt32(Console.ReadLine());
+        }
+
+        // Calculate sum
+        for (int i = 0; i < 5; i++)
+        {
+            sum += marks[i];
+        }
+
+        // Calculate average
+        average = sum / 5.0;
+
+        // Display result
+        Console.WriteLine("Total Marks = " + sum);
+        Console.WriteLine("Average Marks = " + average);
+    }
+}
